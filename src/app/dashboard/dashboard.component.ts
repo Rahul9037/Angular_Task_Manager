3002b760ab6b432015284eff57aef97d38d39eb2
Rahul S Kumar <rahul.a.s.kumar@accenture.com>
2023-10-07 12:59:44 +0530

    static dashboard checkin

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  Designation:string='';
+  Username:string='';
+  NoOfTeamMembers:number=0;
+  TotalCostOfAllProjects:number=0;
+  PendingTask:number=0;
+  UpcomingProjects:number=0;
+  ProjectCost:number=0;
+  CurrentExpenditure:number=0;
+  AvailableFunds:number=0;
 
+  Clients:string[] = [];
+  Projects:string[] = [];
+  Years:number[]=[];
+  TeamMembersSummary:any = [];
+  TeamMembers:any = [];
+
+  ngOnInit(): void {
+    this.Designation='Team Lead';
+    this.Username= 'rahul.a.s.kumar';
+    this.NoOfTeamMembers=50;
+    this.TotalCostOfAllProjects=240;
+    this.PendingTask=5;
+    this.UpcomingProjects=10;
+    this.ProjectCost=120;
+    this.CurrentExpenditure=100;
+    this.AvailableFunds=20;
+  }
+  
 }
